f132ac8e1d5f532b8d94f0409a59bda890b6f640
Oleg Nurzhanov <oleg@logika.dev>
2025-11-17 11:57:09 +0300

    добавил папку с проекта

---
+++ work/pytest/clients.py
@@ -0,0 +1,39 @@
+client_list = [
+    [1, "Олег", "+7 993 565 45 78", "@olegnastyle"],
+    [2, "Дмитрий", "+7 993 657 45 78", "@dima"],
+    [3, "Владимир", "+7 993 897 45 78", "@vova"]
+]
+
+def client_add():
+    print("Список клиентов:👯\n")
+    for client in client_list:
+        print(
+            f"ID: {client[0]}\n"
+            f"Имя: {client[1]}\n"
+            f"Телефон: {client[2]}\n"
+            f"Телеграм: {client[3]}\n"
+        )
+
+def client_read():
+    print("Добавление клиента 👨‍👧\n")
+    user_last_id = len(client_list)+1
+    user_name = input("Имя: ")
+    user_phone = input("Номер телефона: ")
+    user_telegram = input("Телеграм: ")
+    client_list.append([
+        user_last_id,
+        user_name,
+        user_phone,
+        user_telegram
+    ])
+    print(f"Клиент {user_name} был добавлен ✔️")
+
+def client_remove():
+    print("Удаление клиента 👤\n")
+    user_id_remove = int(input("Введите ID клиента: "))
+    user_name_remove = ""
+    for client in client_list:
+        if client[0] == user_id_remove:
+            client_list.remove(client)
+            user_name_remove = client[1]
+            print(f"Клиент {user_name_remove} был удален ❌")
---
+++ work/pytest/main.py
@@ -0,0 +1,24 @@
+from clients import *
+
+while True:
+    print(
+        "1 - посмотреть всех килентов\n"
+        "2 - добавить нового клиента\n"
+        "3 - удалить клиента из списка\n"
+    )
+    value = input("Вы: ")
+
+    if value == "1":
+        print() # пустая строка
+        client_add()
+        print() # пустая строка
+    elif value == "2":
+        print() # пустая строка
+        client_read()
+        print() # пустая строка
+    elif value == "3":
+        print() # пустая строка
+        client_remove()
+        print() # пустая строка
+    else:
+        break
